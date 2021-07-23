6b247860770747ab436b4eb39d00189dc2822130
Brainisthekey <andriialieksieiev@gmail.com>
2021-07-23 18:13:01 +0200

    feat:added total price to the table and redirect buttom

--- core/models.py
+++ core/models.py
@@ -59,6 +59,11 @@ class OrderItem(models.Model):
 
     def get_amount_saved(self):
         return self.get_total_item_price() - self.get_total_item_discount_price()
+    
+    def get_finall_price(self):
+        if self.item.discount_price:
+            return self.get_total_item_discount_price()
+        return self.get_total_item_price()
 
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
@@ -70,6 +75,13 @@ class Order(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_finall_price()
+        return total
+
 
     def __str__(self):
         return self.user.username
