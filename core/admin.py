2faba403a51efcf1d4f97d839470336176050038
Brainisthekey <andriialieksieiev@gmail.com>
2021-07-26 10:39:10 +0200

    feat:changed list display for model DeliveredOrders

--- core/admin.py
+++ core/admin.py
@@ -6,7 +6,6 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ['user', 'ordered', 'billing_adress', 'shipping_adress', 'coupon']
     list_display_links = ['user', 'billing_adress', 'shipping_adress', 'coupon']
-    list_filter = ['ordered']
     search_fields = ['user__username']
 
 class AdressAdmin(admin.ModelAdmin):
@@ -23,10 +22,15 @@ class AdressAdmin(admin.ModelAdmin):
     list_filter = ['default', 'adress_type', 'country']
     search_fields = ['user', 'street_adress', 'apartment_adress', 'zip']
 
+class OrderDevileredAdmin(admin.ModelAdmin):
+
+    #Want to add shipping and billing adress
+    list_display = ['user', 'item_title', 'quantity']
+
 
 admin.site.register(Item)
 admin.site.register(Coupon)
 admin.site.register(OrderItem)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Adress, AdressAdmin)
-admin.site.register(OrderDevilevered)
+admin.site.register(OrderDevilevered, OrderDevileredAdmin)
