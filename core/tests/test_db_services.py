6f55c1804731ecd1571f708cd91014fb9ececf69
Andrii Alieksieiev <andrii.alieksieiev@stxnext.pl>
2021-08-08 11:55:54 +0200

    feat:added db services test

---
+++ core/tests/test_db_services.py
@@ -0,0 +1,9 @@
+from core.services.db_services import filtering_items_by_caegories
+from django.test import TestCase
+
+
+class TestDBCommands(TestCase):
+
+    def test_filtering_items_by_categories(self):
+        """Question there"""
+        pass
