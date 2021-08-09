12ecd0dca4b3e34da20e02105587bb2ac7027e82
Andrii Alieksieiev <andrii.alieksieiev@stxnext.pl>
2021-08-09 18:08:37 +0200

    feat:added a SetUp for testing databases

--- core/tests/__init__.py
+++ core/tests/__init__.py
@@ -1,2 +1,4 @@
 from .test_views import *
 from .test_model import *
+from .test_form_services import *
+from .test_db_services import *
--- core/tests/test_db_services.py
+++ core/tests/test_db_services.py
@@ -1,5 +1,8 @@
 from core.services.db_services import filtering_items_by_caegories
 from django.test import TestCase
+from core.models import Item, OrderItem, Order, Adress, Coupon
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class TestDBCommands(TestCase):
@@ -11,3 +14,58 @@ class TestDBCommands(TestCase):
         Orders, Items, Order Items will create before each tests
         """
         pass
+
+    def setUp(self):
+        self.user = User.objects.create_user('testusername', 'test@test.com', 'testpassword')
+        self.coupon = Coupon.objects.create(code='test', amount=5)
+        self.item = Item.objects.create(
+            title='test',
+            price=30,
+            discount_price=25,
+            description='test',
+            category='R',
+            lable='P',
+            slug='test',
+            image='verity'
+        )
+        self.order_item = OrderItem.objects.create(
+            quantity=1,
+            ordered=False,
+            item=self.item,
+            user=self.user
+        )
+        self.adress = Adress.objects.create(
+            user=self.user,
+            street_adress='test',
+            apartment_adress='test',
+            country='Ukraine',
+            zip='test',
+            adress_type='B',
+            default=True
+        )
+        self.order = Order.objects.create(
+            user=self.user,
+            start_date=timezone.now(),
+            ordered_date=timezone.now(),
+            ordered=False,
+            billing_adress=self.adress,
+            shipping_adress=self.adress,
+            coupon=self.coupon,
+        )
+        self.order.items.add(self.order_item)
+        
+    def test_deleting_order(self):
+
+        print(Order.objects.all())
+        self.order.delete()
+        self.assertEqual(Order.objects.all().count(), 0)
+
+    def test_check_setup_mode(self):
+        
+        #Yeach, it's working for me,
+        #When we run a new tests we SetUp a new enviorenment!
+        self.assertEqual(Order.objects.all().count(), 1)
+    
+        
+
+    
