d854b15e22d26f916da96718722aeaad01e5ba2f
Andrii Alieksieiev <andrii.alieksieiev@stxnext.pl>
2021-08-22 18:48:50 +0200

    feat:added a part of test for views.py

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -3,6 +3,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from core.models import Item, Order, Adress, Coupon, OrderItem
 from django.utils import timezone
+from unittest import mock
 
 
 class TestViews(TestCase):
@@ -51,6 +52,7 @@ class TestViews(TestCase):
             user=cls.user
         )
         cls.order.items.add(cls.order_item)
+
 
     def test_url_allowed_hosts_home_page(self):
         """Test connection to home page"""
@@ -132,6 +134,95 @@ class TestViews(TestCase):
         #Assert error when try to go at the Order Summary page without active order
         self.assertIn('Your shopping cart is empty', html)
 
+#Testing logic in View
+
+    @mock.patch('core.views.add_and_save_coupon_to_the_order')
+    @mock.patch('core.views.get_coupon')
+    @mock.patch('core.views.check_user_for_active_coupon')
+    @mock.patch('core.views.get_order_objects')
+    @mock.patch('core.views.CouponForm')
+    def test_add_coupon_view(
+        self,
+        mock_coupon_form,
+        mock_get_order_object,
+        mock_check_user,
+        mock_get_coupon,
+        mock_add_and_save_coupon
+    ):
+        """Test for AddCouponView"""
+
+        def send_post_add_coupon_request():
+            response = self.c.post(reverse('core:add-coupon'), follow=True, data={'code': 'test_coupon'})
+            return response.content.decode('utf-8')
+        
+        # #Situation where User pass incorect date in form - invalid form
+        mock_coupon_form.return_value = mock.Mock(**{'is_valid.return_value': None})
+        self.assertIn("You do not have an active order", send_post_add_coupon_request())
+
+        #Situation where User has an active coupon
+        mock_coupon_form.return_value = mock.Mock(
+            **{
+                'is_valid.return_value': 'test_success_validation',
+                'cleaned_data.get.return_value' : 'test_coupon',
+            }
+        )
+        mock_get_order_object.return_value = 'test_order'
+        mock_check_user.return_value = 'user_has_active_coupon'
+
+        #Assert that Response which we get from page are correct
+        self.assertIn('You can not use one coupon two times', send_post_add_coupon_request())
+
+        #Situation where User does not have an active coupon
+        mock_check_user.return_value = None
+        mock_get_coupon.return_value = 'test_get_success'
+        mock_add_and_save_coupon.return_value = 'test_add_and_save_coupon'
+
+        #Assert that Response which we get from page are correct
+        self.assertIn('This coupon was successfully added to your order', send_post_add_coupon_request())
+
+        #Situation where User provide a not existent coupon or validation error
+        mock_get_coupon.return_value = None
+
+        #Assert that Response which we get from page are correct
+        self.assertIn('Coupon validation error', send_post_add_coupon_request())
+
+#My templates with filtering are the same
+#I mean that if Romance, Education and Buiseness view has changed only URL
+#And I want to be DRY, and write test for this 3 template in one place
+
+    @mock.patch('core.views.filtering_items_by_caegories')
+    def test_filtering_books(self, mock_filtering_items):
+        """Testing RomanceView"""
+        
+        def send_get_request_to_filtered_home_page(path, context):
+            response = self.c.get(reverse(f'core:{path}'), data={'items': context}, follow=True)
+            return response.content.decode('utf-8')
+        
+        #Get context to pass into teplate
+        mock_items = mock.Mock(
+            **{
+                'image.url': 'test_image',
+                'get_absolute_url' : 'test_path',
+                'get_category_display' : 'test_category',
+                'title' : 'test_title',
+                'discount_price' : 'test_price',
+
+            }
+        )
+        mock_filtering_items.return_value = [mock_items]
+
+        #Iteration by verios path
+        for response_html in [
+            send_get_request_to_filtered_home_page(path='romance', context=mock_filtering_items),
+            send_get_request_to_filtered_home_page(path='education-reference', context=mock_filtering_items),
+            send_get_request_to_filtered_home_page(path='business-investing', context=mock_filtering_items)
+        ]:
+            #Assert rendering template
+            self.assertIn('test_title', response_html)
+            self.assertIn('test_category', response_html)
+            self.assertIn('test_price', response_html)
+            self.assertIn('test_path', response_html)
+            self.assertIn('test_image', response_html)
 
 class TestViewsAnonimousUser(TestCase):
 
